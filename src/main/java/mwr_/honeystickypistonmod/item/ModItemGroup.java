5029636ffc8cd3bc48d9e99c6571880b01840d6c
MWR27 <64335495+MWR27@users.noreply.github.com>
2022-02-12 16:18:03 -0500

    Initial commit

---
+++ src/main/java/mwr_/honeystickypistonmod/item/ModItemGroup.java
@@ -0,0 +1,19 @@
+package mwr_.honeystickypistonmod.item;
+
+import mwr_.honeystickypistonmod.block.ModBlocks;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+
+public class ModItemGroup extends ItemGroup {
+	public static final ModItemGroup HONEY_STICKY_PISTON = new ModItemGroup(ItemGroup.GROUPS.length,
+			"my_first_mod");
+
+	public ModItemGroup(int index, String label) {
+		super(index, label);
+	}
+
+	@Override
+	public ItemStack createIcon() {
+		return new ItemStack(ModBlocks.HONEY_STICKY_PISTON.get());
+	}
+}
